2d6aa867c6fc7cdde80ea18fcc2b559696adca9a
daspete <daspetemail@gmail.com>
2018-02-24 20:58:54 +0100

    config update

---
+++ config/admin.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+
+module.exports = {
+    env: {
+
+    },
+
+    head: {
+        titleTemplate: '%s',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+        ]
+    },
+
+    render: {
+        etag: { strong: true },
+        bundleRenderer: {
+            shouldPreload: (file, type) => {
+                return ['script', 'style', 'font'].includes(type)
+            }
+        }
+    },
+
+    //loading: '',
+
+    css: [],
+
+    router: {
+        middleware: []
+    },
+
+    build: {
+        extractCSS: true,
+
+        vendor: []
+    },
+
+    generate: {
+        routes: []
+    },
+
+    modules: [],
+
+    plugins: [],
+
+
+};
+
